d43909b479d65c83119e34c9fa7220805161d9b7
netcat105 <netcat105@gmail.com>
2024-02-11 21:56:49 +0100

    setups

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,7 @@
 #include <Arduino.h>
 
 
-int step_pin_1 = 2;
-int step_pin_2 = 3;
-int step_pin_3 = 4;
-int step_pin_4 = 5;
+int step_pin[4] = { 2,3,4,5 };
 
 bool step_pin_val[4];
 
@@ -12,11 +9,15 @@ bool step_pin_val[4];
 
 
 // put function declarations here:
-void step() {};
+void step() {}
 
 void setup() {
   // put your setup code here, to run once:
-
+for (int i = 0; i > 4; i++)
+{
+ pinMode(step_pin[i], OUTPUT);
+ i++;
+}
 }
 
 void loop() {
