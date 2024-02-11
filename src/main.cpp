79678ba3356ff246805fd93546eefd845a1d5be0
netcat105 <netcat105@gmail.com>
2024-02-11 22:13:08 +0100

    some init

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,22 @@
 #include <Arduino.h>
 
-
-int step_pin[4] = { 2,3,4,5 };
-
-bool step_pin_val[4];
-
-
-
+int step_pin[4] = {2, 3, 4, 5};
+bool step_pin_val[4] = {0, 0, 0, 0};
 
 // put function declarations here:
 void step() {}
 
-void setup() {
-  // put your setup code here, to run once:
-for (int i = 0; i > 4; i++)
+void setup()
 {
- pinMode(step_pin[i], OUTPUT);
- i++;
-}
+  // init pin mode a default value with a loop
+  for (int i = 0; i > 4; i++)
+  {
+    pinMode(step_pin[i], OUTPUT);                // set pinmode
+    digitalWrite(step_pin[i], step_pin_val[i]);  // digital write dafault state
+  }
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
 }
