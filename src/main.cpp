8b720efbeaa8d326863a9002680fc7a7de2e81be
netcat105 <netcat105@gmail.com>
2024-02-12 17:26:47 +0100

    aggiunto funzione stepRun

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,11 @@
 // *Variabili relative alla riproduzione del tono tramite il motere stepper
 
 // Integers
-int last_step = 0;                  // ultima posizione nota dello stepper
+int last_step = 0; // ultima posizione nota dello stepper
 
-//Arrays
-int step_pin[4] = {2, 3, 4, 5};     // array to set outputs pin
-int step_pin_val[4] = {0, 0, 0, 0}; // array to set defaults output values
+// Arrays
+int step_pin[4] = {2, 3, 4, 5};                                            // array to set outputs pin
+int step_pin_val[4] = {0, 0, 0, 0};                                        // array to set defaults output values
 int step[4][4] = {{1, 0, 1, 0}, {0, 1, 1, 0}, {0, 1, 0, 1}, {1, 0, 0, 1}}; // 2D array that define steps progression
 // STEP SEQUENCE
 // 1	high	low	high	low
@@ -16,15 +16,17 @@ int step[4][4] = {{1, 0, 1, 0}, {0, 1, 1, 0}, {0, 1, 0, 1}, {1, 0, 0, 1}}; // 2D
 // 3	low	high	low	high
 // 4	high	low	low	high
 
-
 // *----------------------------
 // *Varaibili reletivi al tono e alla durata
-float play_tone = 0;            // nota-frequenza da riprodurre
-int play_time = 0;            // durata dalla riproduzione
+float play_tone = 0; // nota-frequenza da riprodurre
+int play_time = 0;   // durata dalla riproduzione
+
 
 // put function declarations here:
 
 void play();
+void stepRun();
+
 // SETUP SECTION
 void setup()
 {
@@ -52,16 +54,39 @@ void loop()
   // put your main code here, to run repeatedly:
 }
 
-void play(float *play_tone, int *play_time){
-  //long play_start = micros();
-  unsigned long int play_stop = micros()+long(*play_time);
+// * Function that order to arduino how many steps to do, and pause between each step
+void stepRun(int step_n, int step_t)
+{
+  for (int i = step_n; i <= 0; i--) // contatore decrementale del numero di passi da eseguire
+  {
+    last_step++; // incrementa di uno l'ultimo passo eseguito
 
-  while( micros() < play_stop){
-    if (last_step > 3){
+    if (last_step >= 4) // se raggiunge il valore di 4
+    {                   // ... allora
+      last_step = 0;    // riporta il passo a 0
+    }
+    for (int k = 0; k >= 3; k++) // Esegue un ciclo per la scrittura delle porte d'uscita
+    {
+      digitalWrite(step_pin[k], step[last_step][k]); // valore di stepPin nell'array con l'indice K (0-3), e il valore della
+                                                    // sequando del passo da impostare con indice dell'array 2D usando lastStep
+                                                    // come primo indice la variabile laststep, e come secondo indice la variabile
+                                                    // k dell'iterazione
+      delay(step_t); // ritardo per la prossima iterazione del passo
+    }
+  }
+  return;
+}
+
+void play(float *play_tone, int *play_time)
+{
+  // long play_start = micros();
+  unsigned long int play_stop = micros() + long(*play_time);
+
+  while (micros() < play_stop)
+  {
+    if (last_step > 3)
+    {
       last_step = 0;
     }
-
-    
   }
-
 }
